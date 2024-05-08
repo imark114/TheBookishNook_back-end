6c5378d526aa987024f0385541fc7743278580ca
rakib114 <mahmud17209@gmail.com>
2024-05-09 00:20:34 +0600

    brrow book date

--- borrow_book/models.py
+++ borrow_book/models.py
@@ -5,7 +5,7 @@ from book.models import Book
 class Borrowing(models.Model):
    coustomer = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
    book = models.ForeignKey(Book, on_delete=models.CASCADE)
-   borrow_date = models.DateTimeField(auto_now_add=True)
+   borrow_date = models.DateField(auto_now_add=True)
    return_date = models.DateField(blank=True, null=True)
    def __str__(self):
        return self.book.title
